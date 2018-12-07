3cb8aee993ec2b0a7db660d5ca96d33e7c8e935f
ellelang <langzx@uw.edu>
2018-12-06 17:43:13 -0800

    research log updated

--- code/r/RPLmodel.R
+++ code/r/RPLmodel.R
@@ -33,17 +33,17 @@ set.seed(123)
 dataset1$scaleimplakes <- dataset1$implakes / 1000
 dataset1$negWetland <- -1 * dataset1$Wetland 
 
-nlogit_rpl.gmnl <- gmnl(Y ~ Payment + negWetland + Covercrop| 
-                   dem_2018  + incfar|
-                   0|
-                 scaleimplakes, 
+nlogit_rpl.gmnl <- gmnl(Y ~ Payment + negWetland + Covercrop | 
+                    dem_2018 + incfar |
+                    0|
+                  scaleimplakes,
                  data = dataset1,
                  model = 'mixl',
                  R = 20,
                  panel = TRUE,
                  ranp = c(negWetland = "ln", Covercrop = "n", Payment = "ln"),
                  mvar = list(Covercrop = "scaleimplakes"),
-                 correlation = TRUE)
+                 correlation = FALSE)
 summary(nlogit_rpl.gmnl)
 
 ################  RPL 
@@ -56,10 +56,70 @@ rpl.gmnl <- gmnl(Y ~ Payment + negWetland + Covercrop|
                   scaleimplakes + incfar_5 + costlive, 
                  data = dataset1,
                  model = 'mixl',
-                 R = 20,
+                 R = 50,
                  panel = TRUE,
                  ranp = c(negWetland = "ln", Covercrop = "n", Payment = "ln"),
                  mvar = list(Covercrop = "scaleimplakes",
                             Payment = c("incfar_5","costlive")),
-                 correlation = TRUE)
+                 correlation = FALSE)
 summary(rpl.gmnl)
+
+
+
+
+#################
+# Why Am I getting NAs with gmnl? https://rpubs.com/msarrias1986/316032
+#dataset1$pay_sdz <- (dataset1$Payment - mean(dataset1$Payment))/sd(dataset1$Payment)
+# rpl.gmnl_sdpay <- gmnl(Y ~ pay_sdz + negWetland + Covercrop| 
+#                    dem_2018  +  foodcost +  
+#                    incfar_1 + incfar_2 + incfar_3 + incfar_4 +
+#                    areaf_1 + areaf_2 + areaf_3 + areaf_4  |
+#                    0|
+#                    scaleimplakes + incfar_5 + costlive, 
+#                  data = dataset1,
+#                  model = 'mixl',
+#                  R = 50,
+#                  panel = TRUE,
+#                  ranp = c(negWetland = "ln", Covercrop = "n", pay_sdz = "ln"),
+#                  mvar = list(Covercrop = "scaleimplakes",
+#                              pay_sdz = c("incfar_5","costlive")),
+#                  correlation = FALSE)
+# summary(rpl.gmnl_sdpay)
+
+
+
+
+
+
+
+
+##################WTP space 
+
+
+wtp.gmnl(mnl.gmnl, wrt = "Payment")
+wtp.gmnl(nlogit_rpl.gmnl, wrt = "Payment")
+
+wtp_dataset1 <- mlogit.data (dataset, varying = 6:43, shape = "long",
+                             choice = "Y", alt.levels = c("V", "C"), 
+                             id.var = "id", opposite = c("Payment"))
+
+wtp_dataset1 $scaleimplakes <- wtp_dataset1 $implakes / 1000
+wtp_dataset1 $negWetland <- -1 * wtp_dataset1 $Wetland
+
+
+start2 <- c(0.1, coef(wtp_nlogit_rpl.gmnl), rep(0.1,5),0.2, 0 )
+wtp_nlogit_rpl.gmnl_1 <- gmnl(Y ~ Payment + negWetland + Covercrop + dem_2018 + incfar + scaleimplakes   | 
+                                0 |
+                                0 |
+                              0|
+                                1,
+                                data = wtp_dataset1,
+                                model = 'gmnl',
+                                R = 50,
+                                panel = TRUE,
+                               ranp = c(negWetland = "ln", Covercrop = "n", Payment = "ln"),
+                               mvar = list(Covercrop = "scaleimplakes"),
+                                fixed = c(TRUE,rep(FALSE,12),TRUE),
+                                start = start2,
+                                iterlim = 100)
+summary(wtp_nlogit_rpl.gmnl)
---
+++ code/r/wta_sim.R
@@ -0,0 +1,58 @@
+rm(list = ls())
+library(mlogit)
+library(gmnl)
+library(tidyverse)
+library(fastDummies)
+setwd("C:/Users/langzx/Desktop/github/DCM/data")
+dataset <- read.csv (file = "wta_observables11192018.csv",header = TRUE)
+dim(dataset)
+n <- dim(dataset)[1]
+colnames(dataset)
+betas <- read.table("beta.dat", sep = "\t", header = TRUE)
+varcov <- read.table("varcov.dat", sep = "\t", header = FALSE)
+
+########Construct X matrix
+var_names <- betas$Names
+var_names
+
+k <- length(var_names)
+wetland <- - 1*dataset0$Wetland
+pay <- dataset$Payment
+cc <- dataset$Covercrop
+nm <-dataset$NuMgt
+asc <- rep(1,n)
+dems18 <- dataset$dem_2018
+costtax <- dataset$taxcost
+##dummyvariables
+inc1 <- dataset$income_1
+inc2 <- dataset$income_2
+inc3 <- dataset$income_3
+inc4 <- dataset$income_4
+inc5 <- dataset$income_5
+inc6 <- dataset$income_6
+inc7 <- dataset$income_7
+farm1 <- dataset$areaf_1
+farm2 <- dataset$areaf_2
+farm3 <- dataset$areaf_3
+farm4 <- dataset$areaf_4
+##interactions
+lakescale <- dataset$implakes/1000
+payinc <- pay * inc7
+cclake <- cc * lakescale
+#########
+cat(paste(var_names, collapse=", "))
+Xmatrix <- as.matrix(cbind (wetland, pay, cc, nm, asc, dems18, 
+                  costtax, inc1, inc2, inc3, inc4, inc5, 
+                  inc6, farm1, farm2, farm3, farm4, payinc, cclake),
+                  ncol = k, nrow = n)
+Xmatrix
+
+X_subset <- Xmatrix[1:100,]
+
+##############################KR estimates
+
+
+
+
+
+dataset <- select(dataset0, -c("id","ChoiceSet","alti","task","income_NA","areaf_NA"))
