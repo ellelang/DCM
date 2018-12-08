6574a93c9b862fc1853d688e5dfb9fdda0bc6f90
ellelang <langzx@uw.edu>
2018-12-07 17:50:35 -0800

    add income data

--- code/r/county_level_models.R
+++ code/r/county_level_models.R
@@ -3,8 +3,15 @@ library(mlogit)
 library(gmnl)
 library(tidyverse)
 library(fastDummies)
+setwd("C:/Users/langzx/Desktop/github/DCM/data")
+dataset <- read.csv (file = "wholeCEscio_dataset_envinfo_1119.csv",header = TRUE)
+county <- dataset$County_resident
+unique(county)
+countyname <- cat(paste(shQuote(unique(county), type="cmd"), collapse=", "))
+county_u <- c ("Hennepin", "Watonwan", "Yellow Medicine", "Brown", "Blue Earth", "Cottonwood", "Todd", "Le Sueur", "Douglas", "Swift", "Redwood", "Otter Tail", "Grant", "Faribault", "Stevens", "Murray", "Waseca", "Lyon", "Sibley", "Freeborn", "Lincoln", "Kandiyohi", "Carver", "Lac Qui Parle", "Traverse", "Dakota", "Rice", "Renville", "McLeod", "Nicollet", "Chippewa", "Martin", "Goodhue", "Pope", "Jackson", "Meeker", "Pipestone", "Nobles", "Wabasha", "Steele", "Big Stone", "NA", "Washington", "Anoka", "Sherburne")
+county_u
 
-dataset <- read.csv (file = "wholeCEscio_dataset_countyinfo_1116.csv",header = TRUE)
+write.table (x = county_u, file = "county.dat", sep = "\t",quote=FALSE, row.names = FALSE)
 dim(dataset)
 
 dataset1 <- mlogit.data(dataset, varying = 5:152, shape = "long",
