62836d318f41162b6bb64d3b94bd59c5b20027b3
Elle Lang <ellelang@users.noreply.github.com>
2019-01-15 11:43:58 -0800

    Updated via the Open Science Framework

--- code/r/wta_sim.R
+++ code/r/wta_sim.R
@@ -1,9 +1,11 @@
 rm(list = ls())
+set.seed(12345)
 library(mlogit)
 library(gmnl)
 library(tidyverse)
 library(fastDummies)
-setwd("C:/Users/langzx/Desktop/github/DCM/data")
+#setwd("C:/Users/langzx/Desktop/github/DCM/data")
+setwd("//udrive.uw.edu/udrive/MRB surveys/Results")
 dataset <- read.csv (file = "wta_observables11192018.csv",header = TRUE)
 dim(dataset)
 n <- dim(dataset)[1]
@@ -11,12 +13,11 @@ dataset$asc <- rep(1,n )
 dataset$lake1000 <- dataset$implakes/1000
 colnames(dataset)
 betas <- read.table("beta.dat", sep = ",", header = TRUE)
-varcov <- read.table("varcov.dat", sep = "\t", header = FALSE)
-L <- t(chol(varcov))
-L
-sd <- read.table("sd.dat", sep = ",", header = TRUE)
+#varcov <- read.table("varcov.dat", sep = "\t", header = FALSE)
+#L <- t(chol(varcov))
+#sd <- read.table("sd.dat", sep = ",", header = TRUE)
 betas$Names
-sd
+betas$Betas
 
 
 
@@ -44,7 +45,7 @@ nm_rp <- betas$Betas[betas$Names=="nm"]
 beta2<- c(0,0,0,0,betas$Betas[5:18],0)
 
 
-## individual samples
+## individual sample 
 dat1 <- sample_n(dat0, 1)
 dat1
 dat_m <- as.matrix(x = dat1, nrow = 1, ncol = 19)  
@@ -53,18 +54,48 @@ dat_m %*% betas$Betas
 
 dat_m %*% beta2
 dat_m
-############WTA estimate
+
+############INDIVIDUAL WTA estimate EXAMPLE
 n_draws <- 10000
+
+# WTA for any program
+wta_anything <- -(as.vector(dat_m %*% beta2)) / pay_rp * rexp(rate = 1, n = n_draws)
+mean(wta_anything, na.rm = TRUE)
+plot(density(wta_anything))
+summary(wta_anything)
+
+# marginal WTA for wetland
+wta_mwetland <- -(wld_rp * rexp(rate = 1, n = n_draws) ) / pay_rp * rexp(rate = 1, n = n_draws)
+mean(wta_mwetland, na.rm = TRUE)
+plot(density(wta_mwetland))
+summary(wta_mwetland)
+
+# marginal WTA for NM
+wta_mnm <- -(nm_rp * rexp(rate = 1, n = n_draws) ) / pay_rp * rexp(rate = 1, n = n_draws)
+mean(wta_mnm, na.rm = TRUE)
+plot(density(wta_mnm))
+summary(wta_mnm)
+
+# marginal WTA for cover crops
+lake_rp
+lake_rp * dat_m[,19]
+wta_cc <- -((cc_rp + lake_rp * dat_m[,19] )* rexp(rate = 1, n = n_draws) ) / pay_rp * rexp(rate = 1, n = n_draws)
+mean(wta_cc, na.rm = TRUE)
+plot(density(wta_cc))
+summary(wta_cc)
+
+# WTAs with 'fixed costs' added
+
 wta_wld <- -(wld_rp * rexp(rate = 1, n = n_draws) + as.vector(dat_m %*% beta2)) / pay_rp * rexp(rate = 1, n = n_draws)
 mean(wta_wld, na.rm = TRUE)
-plot(density(wtp_wld))
-wta_wld
+plot(density(wta_wld))
+summary(wta_wld)
 
 
 wta_nm <- -(nm_rp * rexp(rate = 1, n = n_draws) + as.vector(dat_m %*% beta2)) / pay_rp * rexp(rate = 1, n = n_draws)
 mean(wta_nm, na.rm = TRUE)
 plot(density(wta_nm))
-wta_nm
+summary(wta_nm)
 
 lake_rp
 lake_rp * dat_m[,19]
@@ -74,91 +105,114 @@ plot(density(wta_cc))
 wta_cc
 
 
-###########
+########### FOR WHOLE SAMPLE
+n <- dim(dataset)[1]
+t <- 8
+s <- 441
+index <- seq(1, n, by=16) 
+length(index)
+n_draws <- 10000
 
-#new_beta <- t(L)%*% betas$Betas + betas$Betas
-#new_beta[3,]
+#### 'fixed cost of any program' WTA
+ANY_WTA_ALL <- matrix(NA, nrow = s, ncol = 3)
+colnames(ANY_WTA_ALL) <- c("MEAN", "CI_L","CI_U") # note these aren't confidence intervals but percentiles of taste heterogeneity
 
-normals <- rnorm(10, 0, 1)
-new_beta2 <- c(0,0,0,0,new_beta[5:19])
-#new_beta2_wld <- c(0,0,0,0,new_beta[5:19])
-#new_beta2_wld
-wta_wetland <- -((exp (new_beta[1,] + sd$sd[1] * normals)) + dat_m %*% new_beta2)/ new_beta_price
-wta_wetland
-!is.na(wta_wetland)
-plot(density(na.omit(wta_wetland)))
+for (i in 1:s){
+  index_i <- index(i)
+  dat_s <- dat0[i,]
+  dat_s_m <- as.matrix(x = dat_s, nrow = 1, ncol = 19)  
+  wta_s <- -(as.vector(dat_s_m %*% beta2)) / pay_rp * rexp(rate = 1, n = n_draws)
+  ANY_WTA_ALL[i,1] <- mean(wta_s)
+  ANY_WTA_ALL[i,2] <- quantile(wta_s, 0.05) 
+  ANY_WTA_ALL[i,3] <- quantile(wta_s, 0.95)
+}
+summary(ANY_WTA_ALL)
 
-wta_nm <- -((exp (new_beta[4,] + sd$sd[4] * normals)) + dat_m %*% new_beta2_wld)/ new_beta_price
-plot(density(na.omit(wta_nm)))
-#wta_nm <- mean(- dat_m %*% new_beta2_nm/ new_beta_price,na.rm=TRUE)
-wta_nm
+plot(density (wta_s),xlim = c(-1000,1000))
 
-new_beta2_cc <- c(0,0,new_beta_cc,0,new_beta[5:19])
-wta_cc <- -((exp (new_beta[3,] + sd$sd[3] * normals)) + dat_m %*% new_beta2_wld)/ new_beta_price
-
-plot(density(na.omit(wta_cc)))
-
-wta_cc <- mean(- dat_m %*% new_beta2_cc/ new_beta_price,na.rm=TRUE)
-wta_cc
-######################
-
-beta_price <- exp(-6.41375 + 2.96762 * normals)
-
-
-
-
-new_beta_price <- exp(new_beta[2,] + sd$sd[2] * normals)
-new_beta_price
-new_beta_wld <- exp (new_beta[1,] + sd$sd[1] * normals)
-new_beta_cc <- exp (new_beta[3,]  + sd$sd[3] * normals)
-new_beta_nm <- exp (new_beta[4,] + sd$sd[4] * normals)
-new_beta_nm
-
-b_wetland <- -1.7157
-rd <- rexp(10000000,rate = 1/-1.7157)
-mean(rd)
-########Construct X matrix
-var_names <- betas$Names
-var_names
-
-k <- length(var_names)
-wetland <- - 1*dataset0$Wetland
-pay <- dataset$Payment
-cc <- dataset$Covercrop
-nm <-dataset$NuMgt
-asc <- rep(1,n)
-dems18 <- dataset$dem_2018
-costtax <- dataset$taxcost
-##dummyvariables
-inc1 <- dataset$income_1
-inc2 <- dataset$income_2
-inc3 <- dataset$income_3
-inc4 <- dataset$income_4
-inc5 <- dataset$income_5
-inc6 <- dataset$income_6
-inc7 <- dataset$income_7
-farm1 <- dataset$areaf_1
-farm2 <- dataset$areaf_2
-farm3 <- dataset$areaf_3
-farm4 <- dataset$areaf_4
-##interactions
-lakescale <- dataset$implakes/1000
-payinc <- pay * inc7
-cclake <- cc * lakescale
-#########
-cat(paste(var_names, collapse=", "))
-Xmatrix <- as.matrix(cbind (wetland, pay, cc, nm, asc, dems18, 
-                  costtax, inc1, inc2, inc3, inc4, inc5, 
-                  inc6, farm1, farm2, farm3, farm4, payinc, cclake),
-                  ncol = k, nrow = n)
-Xmatrix
-
-X_subset <- Xmatrix[1:100,]
-
-##############################KR estimates
+#sample average value 
+WLD_mean <- mean(WLD_WTA_ALL[,1])
+WLD_CIL <- mean(WLD_WTA_ALL[,2])
+WLD_CIU <- mean(WLD_WTA_ALL[,3])
+WLD_mean
+WLD_CIL
+WLD_CIU
 
 
+##########wld wta
+WLD_WTA_ALL <- matrix(NA, nrow = s, ncol = 3)
+colnames(WLD_WTA_ALL) <- c("MEAN", "CI_L","CI_U") # note these aren't confidence intervals but percentiles of taste heterogeneity
+
+for (i in 1:s){
+  index_i <- index(i)
+  dat_s <- dat0[i,]
+  dat_s_m <- as.matrix(x = dat_s, nrow = 1, ncol = 19)  
+  wta_s <- -(wld_rp * rexp(rate = 1, n = n_draws) + as.vector(dat_s_m %*% beta2)) / pay_rp * rexp(rate = 1, n = n_draws)
+  WLD_WTA_ALL[i,1] <- mean(wta_s)
+  WLD_WTA_ALL[i,2] <- quantile(wta_s, 0.25) # note these are 1st and 3rd quantiles
+  WLD_WTA_ALL[i,3] <- quantile(wta_s, 0.75)
+}
+summary(WLD_WTA_ALL)
+
+plot(density (wta_s),xlim = c(-1000,1000))
+
+#sample average value 
+WLD_mean <- mean(WLD_WTA_ALL[,1])
+WLD_CIL <- mean(WLD_WTA_ALL[,2])
+WLD_CIU <- mean(WLD_WTA_ALL[,3])
+WLD_mean
+WLD_CIL
+WLD_CIU
+##########cc wta
+CC_WTA_ALL <- matrix(NA, nrow = s, ncol = 3)
+colnames(CC_WTA_ALL) <- c("MEAN", "CI_L","CI_U")
+
+for (i in 1:s){
+  index_i <- index(i)
+  dat_s <- dat0[i,]
+  dat_s_m <- as.matrix(x = dat_s, nrow = 1, ncol = 19)  
+  wta_s <- -((cc_rp + lake_rp * dat_s_m[,19] )* rexp(rate = 1, n = n_draws) + as.vector(dat_s_m %*% beta2)) / pay_rp * rexp(rate = 1, n = n_draws)
+  CC_WTA_ALL[i,1] <- mean(wta_s)
+  CC_WTA_ALL[i,2] <- quantile(wta_s, 0.25)
+  CC_WTA_ALL[i,3] <- quantile(wta_s, 0.75)
+}
+summary(CC_WTA_ALL)
+
+plot(density (wta_s),xlim = c(-1000,1000))
+
+#sample average value 
+CC_mean <- mean(CC_WTA_ALL[,1])
+CC_CIL <- mean(CC_WTA_ALL[,2])
+CC_CIU <- mean(CC_WTA_ALL[,3])
+CC_mean
+CC_CIL
+CC_CIU
+
+##########nm wta
+NM_WTA_ALL <- matrix(NA, nrow = s, ncol = 3)
+colnames(NM_WTA_ALL) <- c("MEAN", "CI_L","CI_U")
+
+for (i in 1:s){
+  index_i <- index(i)
+  dat_s <- dat0[i,]
+  dat_s_m <- as.matrix(x = dat_s, nrow = 1, ncol = 19)  
+  wta_s <- -(nm_rp * rexp(rate = 1, n = n_draws) + as.vector(dat_s_m %*% beta2)) / pay_rp * rexp(rate = 1, n = n_draws)
+  NM_WTA_ALL[i,1] <- mean(wta_s)
+  NM_WTA_ALL[i,2] <- quantile(wta_s, 0.25)
+  NM_WTA_ALL[i,3] <- quantile(wta_s, 0.75)
+}
+summary(NM_WTA_ALL)
+
+plot(density (wta_s),xlim = c(-1000,1000))
+
+#sample average value 
+NM_mean <- mean(NM_WTA_ALL[,1])
+NM_CIL <- mean(NM_WTA_ALL[,2])
+NM_CIU <- mean(NM_WTA_ALL[,3])
+NM_mean
+NM_CIL
+NM_CIU
 
 
 
-dataset <- select(dataset0, -c("id","ChoiceSet","alti","task","income_NA","areaf_NA"))
+
