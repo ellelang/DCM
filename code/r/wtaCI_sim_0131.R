40b611f050b3d05804e380d277411f5408eb6d1c
Elle Lang <langzx@uw.edu>
2019-02-01 07:14:14 -0800

    nlogit random para distributions

--- code/r/wtaCI_sim_0131.R
+++ code/r/wtaCI_sim_0131.R
@@ -35,45 +35,10 @@ lake_rp <- newbetas$Betas[newbetas$Names=="cclake"]
 nm_rp <- newbetas$Betas[newbetas$Names=="nm"]
 length(newbetas$Names)
 
-## fixed parameters
-new_beta2<- c(0,0,0,0,newbetas$Betas[5:17],0)
-
-
-
 dat0 <- select (dataset, Wetland, Payment, Covercrop, NuMgt, asc, 
                 dem_2018,taxcost, 
                 income_1, income_2, income_3, income_4, income_5, income_6,
                 areaf_1, areaf_2, areaf_3, areaf_4, lake1000)
-
-## individual sample 
-dat1 <- sample_n(dat0, 1)
-dat1
-dat_m <- as.matrix(x = dat1, nrow = 1, ncol = 18)  
-dat_m
-
-############INDIVIDUAL WTA estimate EXAMPLE
-draws <- matrix (NA,nrow = 10, ncol = 18)
-colnames(draws)<-c('wetland','pay','cc','nm','asc','dems18','costtax',
-                   'inc1','inc2','inc3','inc4','inc5','inc6',
-                   'farm1','farm2','farm3','farm4','cclake')
-for (r1 in 1 : 10){
-  drawbetas<- betas
-  drawbetas$Betas <- as.vector(rmvnorm(1,mean = betas$Betas, sigma = varcov_m))
-  draws[r1, ] <- drawbetas$Betas
-}
-
-draws
-
-draws[1,"pay"]
-newbetas <- draws[1,]
-newbetas["pay"]
-newbetas[5:17]
-
-n_draws <- 10000
-wta_wld <- -(wld_rp * rexp(rate = 1, n = n_draws) + as.vector(dat_m %*% new_beta2)) / pay_rp * rexp(rate = 1, n = n_draws)
-mean(wta_wld, na.rm = TRUE)
-plot(density(wtp_wld))
-wta_wld
 
 ################## FOR WHOLE SAMPLE
 set.seed(123456)
@@ -160,7 +125,7 @@ for (i in 1:s){
     index_i <- index[i]
     dat_s <- dat0[i,]
     dat_s_m <- as.matrix(x = dat_s, nrow = 1, ncol = 18)  
-    wta_s <- -((cc_rp)* rexp(rate = 1, n = n_draws) + as.vector(dat_s_m %*% new_beta2) + cc_rp*lake_rp * rexp(rate = 1, n = n_draws) ) / pay_rp
+    wta_s <- -((cc_rp + lake_rp)* rexp(rate = 1, n = n_draws) + as.vector(dat_s_m %*% new_beta2) + cc_rp*lake_rp * rexp(rate = 1, n = n_draws) ) / pay_rp
     wta_vec[r] <- mean(wta_s,na.rm = TRUE)
   }
   CC_WTA_ALL[i,1] <- mean(wta_vec,na.rm = TRUE)
