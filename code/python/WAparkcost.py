4509dccd53d923bc666b5eccdca04dc44a66a173
Elle Lang <langzx@uw.edu>
2019-06-25 12:51:49 -0700

    WA cost

---
+++ code/python/WAparkcost.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+from pathlib import Path
+data_folder = Path("C:/Users/langzx/Desktop/github/DCM/data")
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
