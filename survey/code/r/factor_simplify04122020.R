1a0a813cdae1ada58b993da9e5a24eaf10ec8d87
ellelang <ellelang@outlook.com>
2020-04-13 17:02:55 -0700

    plots

--- survey/code/r/factor_simplify04122020.R
+++ survey/code/r/factor_simplify04122020.R
@@ -1,5 +1,6 @@
 rm(list = ls())
-setwd("C:/Users/langzx/Desktop/github/DCM/survey/data")
+#setwd("C:/Users/langzx/Desktop/github/DCM/survey/data")
+setwd("~/Documents/github/DCM/survey/data")
 library(psych)
 library(FactoMineR)
 library(dplyr) 
@@ -24,13 +25,20 @@ poorloading_DICH <- c('otherswetland', 'otherscovercrop','othersnm',
                       'practicegd','practicemintill','practicerb','practicerp')
 
 dich_se <- select(impute_df_dich , -poorloading_DICH) %>% mutate_if(is.factor,as.numeric)
-
+dim(dich_se)
 het.mat <- hetcor(dich_se)$cor
+
+png("../plot/corrplot.png")
 corrplot(cor(het.mat))
+dev.off()
 fa.parallel(het.mat)
+png("../plot/screeplot.png")
 scree(het.mat)
+dev.off()
 fa4_EFA_dich <- fa(het.mat, nfactor = 4,rotate = "varimax")
 fa4_EFA_dich$loadings
+
+fa.diagram(fa4_EFA_dich)
 
 f_4_CFAmodel <- 'aware =~ obssediment + obsnutrients + obsodor + obstrash + obslackfish + obsunsafeswim + obscolor + obsunsafedrink 
 past =~ pastcrp + pastfcp + pastmci + pastlongterm + pastgcdt + pastgcsv 
@@ -40,10 +48,11 @@ resp =~ sptlandowners + sptfarmmanager + sptrenters + sptgovstaff + sptmrbboard'
 bq_cfa <- cfa(model = f_4_CFAmodel,
               data = dich_se)
 
+png("../plot/cfaplot.png")
 semPaths(object = bq_cfa,
          whatLabels = "std",
          edge.label.cex = 1)
-
+dev.off()
 summary(bq_cfa, standardized = T, fit.measures = T)
 csat_scores <- as.data.frame(predict(bq_cfa)) %>% mutate (practice_ind = practice_indicator1)
 names(csat_scores )
@@ -51,3 +60,33 @@ dim(csat_scores)
 fit <- glm(practice_ind~aware+past+appreciate+resp,data=csat_scores, family = binomial())
 summary(fit)
 pra_predict <- as.vector(predict(fit))
+
+#####
+library(tidyverse)
+library(likert)
+impute_df<- read.csv('factor_impute.csv')
+diffscales <- c('opwetlandopen',
+                  'opwetlandrestored','opcovercropplant',
+                  'opnmopen','opcovercropopen' )
+likert_data <- select (impute_df, -diffscales)
+colnames (likert_data)
+ecosy <- likert_data[,1:15]
+value <- likert_data[,c(16:25,27)]
+names(value)
+practice <- likert_data[,28:47]
+library(dplyr)
+eco_likert <- ecosy %>% mutate_if(is.integer, as.factor) %>% likert()
+val_likert <- value %>% mutate_if(is.integer, as.factor) %>% likert()
+prac_likert <- practice %>% mutate_if(is.integer, as.factor) %>% likert()
+
+png("../plot/ecolikertplot.png")
+plot(eco_likert)
+dev.off()
+
+png("../plot/vallikertplot.png")
+plot(val_likert)
+dev.off()
+
+png("../plot/praclikertplot.png")
+plot(prac_likert)
+dev.off()
